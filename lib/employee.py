22c9b2d2c961ad8090d85c2ac38e07fa63b6c904
philip dutra <monsterphilip10@gmail.com>
2023-09-03 22:13:17 -0400

    completed lab

--- lib/employee.py
+++ lib/employee.py
@@ -2,6 +2,7 @@
 from __init__ import CURSOR, CONN
 from department import Department
 
+
 class Employee:
 
     # Dictionary of objects saved to the database.
@@ -187,4 +188,12 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review  import Review
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+            """
+        CURSOR.execute(sql, (self.id,))
+
+        rows = CURSOR.fetchall()
+        return [Review.instance_from_db(row) for row in rows]
